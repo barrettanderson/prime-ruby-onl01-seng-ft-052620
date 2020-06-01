6caf8eee141cbefe897278bb398fd648243966e9
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-01 16:09:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
 # Add  code here!
 def prime?(number)
-
+  first = 2
+  if number > 1
+    range = (start...first-1).to_a
+    range.none? do |test_number|
+      number % test_number == 0
+    end
+  else
+    false
+  end
 end
